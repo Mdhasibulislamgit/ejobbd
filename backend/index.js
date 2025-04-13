2f3e6d071fbaf31a8f3d2f5a0d4dac5b160a6677
mdhasibulislamgit <mhislamemail@gmail.com>
2025-04-13 15:09:30 +0600

    feat: add root endpoint to check server status
    This commit adds a root endpoint ("/") to the backend server to provide a simple response indicating that the server is working. This is useful for quick health checks and ensuring the server is operational.

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,9 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+app.get("/",(req,res)=>{
+    res.send("Hi there! Server is Working"); 
+})
 
 
 
